d4bcae2815e15dbeea2e23c5e3fc24c8b09f326a
Marko Aleksic <psybaron@gmail.com>
2016-05-19 15:14:35 +0200

    fixed footer

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -1,7 +1,9 @@
 <footer class="container">
     <div class="row">
         <div class="col-md-12">
-            <small style="text-transform: uppercase;">{{ setting('foundation::footer-copy') ? setting('foundation::footer-copy')  : setting('core::site-name') }}</small>
+            <small style="text-transform: uppercase;">
+                {{ setting('foundation::footer-copy') ? setting('foundation::footer-copy')  : setting('core::site-name') }}
+            </small>
         </div>
     </div>
 </footer>
